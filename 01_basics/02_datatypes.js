e3eae80bbc6f4a36596261624b59ada158600009
NikhilHTrivedi <144768155+NikhilHTrivedi@users.noreply.github.com>
2023-09-12 08:33:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code ads newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 
+    3) // code readability should be high
+
+
+console.log("Nikhil")
+
+let name = "Nikhil"
+let age = 26
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint  => for trading, stoke market, facebook, redit
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => the undefined value 
+// symbol => unique
+
+// object
+ 
+console.log(typeof undefined);  // output = undefined
+console.log(typeof null);  // output = object
+
