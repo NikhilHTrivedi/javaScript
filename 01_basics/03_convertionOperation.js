4f6065fadee589a5d780a72a6ba22be0a6ac27d0
NikhilHTrivedi <144768155+NikhilHTrivedi@users.noreply.github.com>
2023-09-12 11:34:54 +0000

    Operation on datatype

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -28,5 +28,45 @@ let someNumber = 55
 
 let stringNumber = String (someNumber)
 
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+// ********************* Operations *************************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);   output => 4         addtion
+// console.log(2-2);   output => 0         subtraction
+// console.log(2*2);   output => 4         multiplication / squre
+// console.log(3**3);  output => 27        cube
+// console.log(2/3);   output => 0.66666   divide
+// console.log(2%3);   output => 2         reminder
+
+let str1 = "Nikhil"
+let str2 = " Trivedi"
+
+let str3 = str1 + str2
+console.log(str3);
+ //                                output
+// console.log("1" + 2);          12
+// console.log(1 + "2");            12
+// console.log("1" + 2 + "2");       122
+// console.log(1 + 2 + "2");        32
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);     1
+// console.log(true+);     error
+// console.log(+"");       0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+//++gameCounter;   prefix
+//gameCounter++;   postfix
+//console.log(gameCounter);
