ca841705b3041fd070c174113f5fb1acae50d9f2
NikhilHTrivedi <144768155+NikhilHTrivedi@users.noreply.github.com>
2023-09-12 10:31:54 +0000

    convertion of datatype

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,32 @@
+let score = "Nikhil"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber );
+//console.log(valueInNumber);
+
+// "33" => 33 output = number
+// "33a" =>  output = NaN
+//true => outpur = 1
+//false=> output = 0
+
+let isLoggedIn = "Nikhil"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => output = true
+// 0 => output = false
+// "" => output = false
+// " " => output = true 
+// "Nikhil" output = true 
+// 5 => output = true 
+
+let someNumber = 55
+
+let stringNumber = String (someNumber)
+
+console.log(stringNumber);
+console.log( typeof stringNumber);
