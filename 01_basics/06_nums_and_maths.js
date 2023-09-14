83220814feef4d0092655ba1de9564358cba46d7
NikhilHTrivedi <144768155+NikhilHTrivedi@users.noreply.github.com>
2023-09-14 07:21:19 +0000

    nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,19 @@
+const score = 500
+// console.log(score);
+const balance = new Number(300)
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 32.6445
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 300000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-10));
