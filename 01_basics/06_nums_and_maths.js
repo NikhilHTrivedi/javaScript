213703102ae1a1ab750529c2ff1ab1510bf5a100
NikhilHTrivedi <144768155+NikhilHTrivedi@users.noreply.github.com>
2023-09-14 08:18:41 +0000

    maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -13,7 +13,26 @@ const otherNumber = 32.6445
 const hundred = 300000000
 // console.log(hundred.toLocaleString('en-IN'));
 
-// ++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+// +++++++++++++++++++++ Maths +++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-10));
+// console.log(Math);
+// console.log(Math.abs(-10));   -ve convet into +ve
+// console.log(Math.round(4.5));  round of any number 4.5 is 5
+
+// console.log(Math.ceil(5.6));   type of round of but this give only up value 5.6 is 6
+
+// console.log(Math.floor(9.5));   type of round of but this give lower value 9.5 is 9
+
+// console.log(Math.min(54,1,654,68,68,6));  // min = 1
+
+// console.log(Math.max(465,5,654456,3212,652,54,64)); mix = 654456
+
+console.log(Math.random());
+console.log((Math.random()*10)+ 1);
+console.log(Math.floor(Math.random()*10)+ 1);
+
+const min = 10  
+const max = 20
+
+// ************* very Importent formula ****************
+console.log(Math.floor(Math.random() * (max - min +1 )) + min);
